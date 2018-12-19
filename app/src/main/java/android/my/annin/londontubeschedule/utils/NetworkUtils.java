79bc671a5da7c5acf7e64d2a2534ac2ccafab719
Maino96-10022 <anast3222@gmail.com>
2018-12-18 23:18:06 -0500

    upload: refactor

--- app/src/main/java/android/my/annin/londontubeschedule/utils/NetworkUtils.java
+++ app/src/main/java/android/my/annin/londontubeschedule/utils/NetworkUtils.java
@@ -10,7 +10,11 @@ public class NetworkUtils
     private static final String API_KEY = "api_key";
     private static final String APP_ID = "ap_id";
 
-    private static final String BASE_URL = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
+    private static final String BASE_URL_LINES_LIST = "https://api.tfl.gov.uk/swagger/ui/index.html?url=/swagger/docs/v1#!/Line/Line_GetByMode";
+
+    private static final String BASE_URL_STATIONS_LIST = "https://api.tfl.gov.uk/swagger/ui/index.html?url=/swagger/docs/v1#!/Line/Line_RouteSequence";
+
+    private static final String BASE_URL_SCHEDULE = "https://api.tfl.gov.uk/swagger/ui/index.html?url=/swagger/docs/v1#!/Line/Line_Arrivals";
 
     public NetworkUtils()
     {
