50f97996f02c18a5c847803784156e74ba038a68
Maino96-10022 <anast3222@gmail.com>
2018-12-23 00:58:08 -0500

    upload: refactor

--- app/src/main/java/android/my/annin/londontubeschedule/ui/StationListActivity.java
+++ app/src/main/java/android/my/annin/londontubeschedule/ui/StationListActivity.java
@@ -3,13 +3,23 @@ package android.my.annin.londontubeschedule.ui;
 import android.my.annin.londontubeschedule.R;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class StationListActivity extends AppCompatActivity
 {
+    @BindView(R.id.tube_list_instructions)
+    TextView stationListInstructions;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_station_list);
+
+        // Bind the views
+        ButterKnife.bind(this);
     }
 }
