efa2821bf4358bc8694d47fa400576cebceca9f3
Maino96-10022 <anast3222@gmail.com>
2018-12-31 21:53:02 -0500

    upload: refactor

--- app/src/main/java/android/my/annin/londontubeschedule/ui/MainActivity.java
+++ app/src/main/java/android/my/annin/londontubeschedule/ui/MainActivity.java
@@ -3,6 +3,7 @@ package android.my.annin.londontubeschedule.ui;
 import android.my.annin.londontubeschedule.R;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -22,4 +23,19 @@ public class MainActivity extends AppCompatActivity
         ButterKnife.bind(this);
 
     }
+
+
+//    //Display if there is no internet connection
+//    public void showErrorMessage()
+//    {
+//        Snackbar
+//                .make(mCoordinatorLayout, "Please check your internet connection", Snackbar.LENGTH_INDEFINITE)
+//                .setAction("Retry", new MyClickListener())
+//                .show();
+//        mRecyclerView.setVisibility(View.INVISIBLE);
+//        mLoadingIndicator.setVisibility(View.VISIBLE);
+//    }
+
+
+
 }
