63c3fcb6fffe37968d0ce001394c70f9eb22fc4c
Maino96-10022 <anast3222@gmail.com>
2019-02-08 00:05:21 -0500

    update code : refactor

--- app/src/main/java/capstone/my/annin/londontubeschedule/timberlog/TimberApplication.java
+++ app/src/main/java/capstone/my/annin/londontubeschedule/timberlog/TimberApplication.java
@@ -2,18 +2,27 @@ package capstone.my.annin.londontubeschedule.timberlog;
 
 import android.app.Application;
 
+import capstone.my.annin.londontubeschedule.BuildConfig;
 import timber.log.Timber;
 
 public class TimberApplication extends Application
 {
-    //Based on the code in this YouTube video:
+    //Based on the code in these links:
     //https://www.youtube.com/watch?v=0BEkVaPlU9A&t=1s&list=LLC3tmBcY0VaQGiTyNWKN70g&index=2
+    //https://www.androidhive.info/2018/11/android-implementing-logging-using-timber/
 
     @Override
     public void onCreate()
     {
         super.onCreate();
 
-        Timber.plant(new Timber.DebugTree());
+        if (BuildConfig.DEBUG)
+        {
+            Timber.plant(new Timber.DebugTree());
+
+        } else
+            {
+        Timber.plant(new ReleaseTree());
+    }
     }
 }
