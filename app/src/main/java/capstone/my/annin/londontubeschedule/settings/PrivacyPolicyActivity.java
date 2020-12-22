78d03fc76a9485b414540ae5e19d8f9cdf40e42a
ANNIN_ST_PC <anast3222@gmail.com>
2020-12-22 16:35:56 -0500

    update code: SettingsActivity license add

--- app/src/main/java/capstone/my/annin/londontubeschedule/settings/PrivacyPolicyActivity.java
+++ app/src/main/java/capstone/my/annin/londontubeschedule/settings/PrivacyPolicyActivity.java
@@ -1,3 +1,17 @@
+/* Copyright 2020 Anastasia Annin
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package capstone.my.annin.londontubeschedule.settings;
 
 import android.content.Context;
@@ -6,21 +20,11 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.maps.OnMapReadyCallback;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import capstone.my.annin.londontubeschedule.R;
-import capstone.my.annin.londontubeschedule.asynctask.AllLinesAsyncTaskInterface;
 import capstone.my.annin.londontubeschedule.asynctask.PrivacyPolicyAsyncTask;
 import capstone.my.annin.londontubeschedule.asynctask.PrivacyPolicyAsyncTaskInterface;
-import capstone.my.annin.londontubeschedule.asynctask.TubeGeoJsonAllLinesAsyncTask;
 
 public class PrivacyPolicyActivity extends AppCompatActivity implements PrivacyPolicyAsyncTaskInterface
 {
--- app/src/main/java/capstone/my/annin/londontubeschedule/settings/SettingsActivity.java
+++ app/src/main/java/capstone/my/annin/londontubeschedule/settings/SettingsActivity.java
@@ -1,22 +1,27 @@
+/* Copyright 2020 Anastasia Annin
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package capstone.my.annin.londontubeschedule.settings;
 
-import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
