8e2e38f63f8283e2e4be34444d69ec322c16595a
ANNIN_ST_PC <anast3222@gmail.com>
2021-01-30 03:59:35 -0500

    update code: TubeLineFragment Interface

---
+++ app/src/main/java/capstone/my/annin/londontubeschedule/ui/ShowSnackbar.java
@@ -0,0 +1,8 @@
+package capstone.my.annin.londontubeschedule.ui;
+
+public interface ShowSnackbar
+{
+  //Fragment to activity interface code based on the following stackoverflow example:
+  //https://stackoverflow.com/questions/65117300/how-to-use-interface-to-communicate-between-fragment-and-activity
+  void showSnackbar();
+}
