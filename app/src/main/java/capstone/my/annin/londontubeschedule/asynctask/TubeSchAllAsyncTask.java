c31d435c124a6b6ab90f20980cef9f7108a3b515
ANNIN_ST_PC <anast3222@gmail.com>
2021-06-02 05:50:07 -0400

    update code: tubeSchAllAsyncTask

--- app/src/main/java/capstone/my/annin/londontubeschedule/asynctask/TubeSchAllAsyncTask.java
+++ app/src/main/java/capstone/my/annin/londontubeschedule/asynctask/TubeSchAllAsyncTask.java
@@ -1,3 +1,18 @@
+/* Copyright 2020 Anastasia Annin
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package capstone.my.annin.londontubeschedule.asynctask;
 
 import android.os.AsyncTask;
@@ -5,7 +20,6 @@ import android.os.AsyncTask;
 import java.net.URL;
 import java.util.ArrayList;
 
-import capstone.my.annin.londontubeschedule.pojo.OvergroundSchedule;
 import capstone.my.annin.londontubeschedule.pojo.Schedule;
 import capstone.my.annin.londontubeschedule.utils.JSONUtils;
 import capstone.my.annin.londontubeschedule.utils.NetworkUtils;
